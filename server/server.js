89c18f33b642f0c5ea176fbf439d11a367576856
potatoyum <hellokyk20@gmail.com>
2020-04-18 01:01:16 +0900

    mongodb 연결
    db 연결

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 
 
 var express = require('express');
+var mongoose = require('mongoose');
 var app = express();
 var server = app.listen(3000, function(){
     console.log("aaaaaaa");
@@ -9,3 +10,13 @@ var server = app.listen(3000, function(){
 app.get('/', function(req,res){
     res.send("hellooooooooo");
 })
+
+var db = mongoose.connection;
+db.on('error', console.error);
+
+db.once('open', function(){
+    // CONNECTED TO MONGODB SERVER
+    console.log("Connected to mongod server");
+});
+
+mongoose.connect('mongodb+srv://cdp12:cdp12@cdp12-eqgsf.mongodb.net/test?retryWrites=true&w=majority');
