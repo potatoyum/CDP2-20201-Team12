d507f65fe6d0f2d9c4cb878d540f4931818ca6bf
pse71 <psepse7171@gamil.com>
2020-04-23 21:32:00 +0900

    디비완료
    완료

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const mongoose = require('mongoose');
 var app = express();
 var server = app.listen(3000, function(){
     console.log("aaaaaaa");
@@ -28,10 +29,18 @@ mongoose.connect('mongodb+srv://cdp12:cdp12@cdp12-eqgsf.mongodb.net/test?retryWr
 });
 
 var peopleSchema = mongoose.Schema({ //스키마 생성
-    name:{
-        type: String,
-    },
-    age: Number
+    id: {
+        type: Number,
+        required: true,
+      },
+    count: {
+        type: Number,
+        required: true,
+      },
+    date: {
+        type: Date,
+        default: Date.now,//나중에 
+      },
 })
 
 module.exports = mongoose.model('people',peopleSchema);
@@ -39,12 +48,12 @@ module.exports = mongoose.model('people',peopleSchema);
 var Person = mongoose.model('people',peopleSchema); //피플스키마의 모델 생성
 
 var pse= new Person({ 
-    name: "pse",
-    age: 21
+    id: 2,
+    count: 30
 })
-
 pse.save(function(err,pse){ //디비에 저장
     if(err) return;
 
     //console.dir(pse);
 })
+
