fc04a3c6ddeb53786db9584d6aac10563a529c34
yallyyally <jhshin0028@gmail.com>
2020-05-16 19:40:39 +0900

    실시간 유동인구 수정 + 구조 수정

--- src/views/Dashboard/DynamicDoughnut.js
+++ src/views/Dashboard/DynamicDoughnut.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Doughnut} from 'react-chartjs-2';
+import CountUp from 'react-countup';
 
 class DynamicDoughnut extends Component {
 
@@ -10,15 +11,28 @@ class DynamicDoughnut extends Component {
 getRandomInt = (min,max)=>{
    return  Math.floor(Math.random()*(max-min+1))+min;
 };
-
+//이거 서버랑 연동
+getSeoPeople = ()=>{
+  return  12;
+}
+getBukPeople = ()=>{
+  return  10;
+}
+getJeongPeople = ()=>{
+  return  13;
+}
+getTotal = () =>{
+  return this.getSeoPeople()+this.getBukPeople()+this.getJeongPeople();
+}
  getState = () => ({
+
   labels: [
-    '서문',
     '북문',
+    '서문',
     '정문'
   ],
   datasets: [{
-    data: [this.getRandomInt(50, 200), this.getRandomInt(100, 150), this.getRandomInt(150, 250)],
+    data: [this.getBukPeople(), this.getSeoPeople(), this.getJeongPeople()],
     backgroundColor: [
     '#CCC',
     '#36A2EB',
@@ -46,10 +60,22 @@ componentWillMount() {
   render() {
     return (
       <div>
-        <h2 style = {{marginLeft:500+'px'}}><strong>실시간 경북대 인근 유동인구</strong></h2>
-        <div className="chart-wrapper" style={{ height: 480 + 'px', marginTop: 30 + 'px',marginLeft: 500+'px' }}>
-        <Doughnut data={this.state} options = {this.option} height = {400}/>
+        <h2 style = {{marginLeft:350+'px'}}><strong>실시간 경북대 인근 유동인구</strong></h2><br/>
+        <div style={{ height: 480 + 'px', marginTop: 30 + 'px',marginLeft: 380+'px',float:'left'}}>
+        <Doughnut data={this.state} options = {this.option} height = {400} />
         </div>
+        <div style = {{float:'left',marginLeft: 50+'px',marginTop: 100+'px',fontSize: 30+'px'}}> 
+        <div style = {{fontSize: 25+'px'}}>
+        최근 한시간 동안 
+        </div>
+        <div style={{height:50+'px', width:100+'px',backgroundColor : 'white',textAlign:'center',borderRadius:12+'px',marginTop:10+'px',marginBottom:10+'px'}}>
+        <CountUp start={0} end={this.getTotal()} />
+        명</div>
+        <div style = {{fontSize: 25+'px'}} >
+        의 유동인구가 있습니다.
+          </div> 
+        </div>
+        {/*여기에 증가 감소 이런거 추가 */}
       </div>
     );
   }
