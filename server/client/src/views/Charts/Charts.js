7026e1921774dcd8654780831360926a6d29f008
potatoyum <hellokyk20@gmail.com>
2020-05-26 19:12:16 +0900

    Update Charts.js

--- server/client/src/views/Charts/Charts.js
+++ server/client/src/views/Charts/Charts.js
@@ -1,163 +1,111 @@
 import React, { Component } from 'react';
-import { Bar, Doughnut, Line, Pie, Polar, Radar } from 'react-chartjs-2';
+import { Bar, Doughnut, Line, Pie, Polar, Radar, defaults } from 'react-chartjs-2';
 import { Card, CardBody, CardColumns, CardHeader, Row, Col } from 'reactstrap';
 import { CustomTooltips } from '@coreui/coreui-plugin-chartjs-custom-tooltips';
-import DatePickerInput from '../DatePicker/DatePickerInput';
+//import DatePickerInput from '../DatePicker/DatePickerInput';
+
+import "react-datepicker/dist/react-datepicker.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import DatePicker from 'react-datepicker';
 import {
   Dropdown,
-  ButtonDropdown,
-  ButtonGroup,
   DropdownItem,
   DropdownMenu,
   DropdownToggle,
+  Button
 } from 'reactstrap';
 
-const line = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: 'rgba(75,192,192,0.4)',
-      borderColor: 'rgba(75,192,192,1)',
-      borderCapStyle: 'butt',
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(75,192,192,1)',
-      pointBackgroundColor: '#fff',
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
-      data: [65, 59, 80, 81, 56, 55, 40],
-    },
-  ],
-};
+const ExampleCustomInput = ({ value, onClick }) => (
+  <Button color="secondary" className="example-custom-input" onClick={onClick}>
+    {value}
+  </Button>
+);
 
-const bar = {
-  labels: ['0-1', '1-2', '2-3', '3-4', '4-5', '5-6', '6-7','7-8','8-9','9-10','10-11','11-12','12-13','13-14','14-15','15-16','16-17','17-18','18-19','19-20','20-21','21-22','22-23','23-24'],
-  datasets: [
-    {
-      label: '유동인구 수',
-      backgroundColor: 'rgba(255,99,132,0.2)',
-      borderColor: 'rgba(255,99,132,1)',
-      borderWidth: 1,
-      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-      hoverBorderColor: 'rgba(255,99,132,1)',
-      data: [65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40, 65, 59, 80, 81, 56, 55, 40, 50, 50, 50],
-    },
-  ],
-};
-
-const doughnut = {
-  labels: [
-    'Red',
-    'Green',
-    'Yellow',
-  ],
-  datasets: [
-    {
-      data: [300, 50, 100],
-      backgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56',
-      ],
-      hoverBackgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56',
-      ],
-    }],
-};
-
-const radar = {
-  labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
-  datasets: [
-    {
-      label: 'My First dataset',
-      backgroundColor: 'rgba(179,181,198,0.2)',
-      borderColor: 'rgba(179,181,198,1)',
-      pointBackgroundColor: 'rgba(179,181,198,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(179,181,198,1)',
-      data: [65, 59, 90, 81, 56, 55, 40],
-    },
-    {
-      label: 'My Second dataset',
-      backgroundColor: 'rgba(255,99,132,0.2)',
-      borderColor: 'rgba(255,99,132,1)',
-      pointBackgroundColor: 'rgba(255,99,132,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(255,99,132,1)',
-      data: [28, 48, 40, 19, 96, 27, 100],
-    },
-  ],
-};
-
-const pie = {
-  labels: [
-    'Red',
-    'Green',
-    'Yellow',
-  ],
-  datasets: [
-    {
-      data: [300, 50, 100],
-      backgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56',
-      ],
-      hoverBackgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56',
-      ],
-    }],
-};
-
-const polar = {
-  datasets: [
-    {
-      data: [
-        11,
-        16,
-        7,
-        3,
-        14,
-      ],
-      backgroundColor: [
-        '#FF6384',
-        '#4BC0C0',
-        '#FFCE56',
-        '#E7E9ED',
-        '#36A2EB',
-      ],
-      label: 'My dataset' // for legend
-    }],
-  labels: [
-    'Red',
-    'Green',
-    'Yellow',
-    'Grey',
-    'Blue',
-  ],
-};
+class DatePick extends React.Component {
+  state = {
+    startDate: new Date(),
+  };
+ 
+  handleChange = date => {
+    this.setState({
+      startDate: date
+    });
+    
+    //console.log(this.state.startDate.getMonth() + "월" + this.state.startDate.getDate() + "일");
+  };
+ 
+  render() {
+    //console.log((this.state.startDate.getMonth() + 1) + "월" + this.state.startDate.getDate() + "일 선택됨");
+   
+    this.props.onClick(this.state.startDate);
+    //console.log(this.state.startDate);
+    return (
+      <DatePicker
+        selected={this.state.startDate}
+        onChange={this.handleChange}
+        maxDate={new Date()}
+        dateFormat="yyyy년 MM월 dd일"
+        className="form-control"
+        customInput={<ExampleCustomInput/>}
+        //onClick={function(e) {
+        //  this.props.onClick(e.target.selected);
+        //}.bind(this)}
+      />
+    );
+  }
+}
 
 const options = {
   tooltips: {
     enabled: false,
     custom: CustomTooltips
   },
-  maintainAspectRatio: false
+  scales: {
+    yAxes: [{
+        ticks: {
+            beginAtZero:true,
+            min: 0,  
+        }
+      }]
+   },
+  maintainAspectRatio: false,
+  legend: {
+    onClick: null
+  }
+}
+
+class DropDownItem extends Component{
+  state = {
+    list:[]
+  }
+  componentDidMount(){
+    fetch('list.json')
+      .then(function(result){
+        return result.json();
+      })
+      .then(function(json){
+        console.log(json);
+        this.setState({list:json});
+      }.bind(this));
+  }
+  render() {
+    var arr = [];
+    for (var i = 0; i < this.state.list.length; i++) {
+      var li = this.state.list[i];
+      arr.push(<DropdownItem key={li.id} data-id={li.id} data-title={li.title} onClick={function (e) {
+        e.preventDefault();
+        this.props.onClick(e.target.dataset.id, e.target.dataset.title);
+      }.bind(this)}>
+        {li.title}
+      </DropdownItem>)
+    }
+    return (
+      <div>
+        {arr}
+      </div>
+
+    );
+  }
 }
 
 class Charts extends Component {
@@ -165,13 +113,30 @@ class Charts extends Component {
     super(props);
 
     this.toggle = this.toggle.bind(this);
+  
     this.state = {
       dropdownOpen: false,
-      //radioSelected: 2,
-      //date: date
+      dropDownValue: '지역 선택',
+      list:[],
+      cam_id: 0,
+      date: '',
+      bar: {
+        labels: ['0-1', '1-2', '2-3', '3-4', '4-5', '5-6', '6-7', '7-8', '8-9', '9-10', '10-11', '11-12', '12-13', '13-14', '14-15', '15-16', '16-17', '17-18', '18-19', '19-20', '20-21', '21-22', '22-23', '23-24'],
+        datasets: [
+          {
+            label: '유동인구 수',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+            borderColor: 'rgba(255,99,132,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+            data: [],
+          },
+        ],
+      }
     };
+    
   }
-
   toggle() {
     this.setState({
       dropdownOpen: !this.state.dropdownOpen,
@@ -181,120 +146,119 @@ class Charts extends Component {
     return (
       <div className="animated fadeIn">
         <Row>
+        <Col>
+            <DatePick onClick={() => function(startDate){
+              //
+              var strdate, year, month, date = '';
+              year = startDate.getFullYear();
+              if ((startDate.getMonth() + 1) < 10)
+                month = '0' + (startDate.getMonth() + 1);
+              else
+                month = '' + (startDate.getMonth() + 1);
+              if (startDate.getDate() < 10)
+                date = '0' + (startDate.getDate());
+              else
+                date = '' + startDate.getDate();
+
+              strdate = year + month + date;
+              console.log(strdate);
+              //console.log(startDate.getFullYear() + "년 " + (startDate.getMonth()+1) + "월 " + startDate.getDate() + "일");
+
+              //this.setState({
+              //  dropdownOpen: this.state.dropdownOpen,
+              //  dropDownValue: this.state.dropDownValue,
+              //  list: this.state.list,
+              //  cam_id: this.state.cam_id,
+              //  date: strdate,
+              //  bar: this.state.bar
+              //});
+              console.log(this.state.date + '// data state value');
+            }.bind(this)} />
+          </Col>
           <Col>
             <Dropdown id='card1' isOpen={this.state.card1} toggle={() => { this.setState({ card1: !this.state.card1 }); }} style={{marginLeft: 500 + 'px' }}>
-              <DropdownToggle caret>
-              지역 선택
+              <DropdownToggle caret style={{width: 100 + 'px'}}>
+                {this.state.dropDownValue}
               </DropdownToggle>
               <DropdownMenu>
-                <DropdownItem disabled>선택 안함</DropdownItem>
+                <DropdownItem disabled>지역 선택</DropdownItem>
                 <DropdownItem divider />
-                <DropdownItem>
-                    <div onClick={() => console.log('This will fire!')}>
-                      북문
-                    </div>
-                </DropdownItem>
-                <DropdownItem>동문</DropdownItem>
-                <DropdownItem>서문</DropdownItem>
-                <DropdownItem>정문</DropdownItem>
+                <DropDownItem onClick={function(id, title){
+                  console.log(id);
+                  //this.setState({cam_id: id});
+                  //console.log(this.state.cam_id + '클릭클릭');
+                  fetch(id + '_' + this.state.date + '.json')
+                    .then(function(result){
+                      return result.json();
+                    })
+                    .then(function(json){
+                      
+                      console.log(id + '_' + this.state.date + '.json');
+                      this.setState({list:json});
+                        var li = this.state.list;
+                        var count = li.count;
+                      this.setState({
+                        dropdownOpen: this.state.dropdownOpen,
+                        dropDownValue: title,
+                        list: this.state.list,
+                        cam_id: id,
+                        date: this.state.date,
+                        bar: {
+                          labels: this.state.bar.lables,
+                          datasets: [
+                            {
+                              label: this.state.bar.datasets[0].label,
+                              data: count,
+                            },
+                          ],
+                        }
+                      })
+                      
+                      //console.log(this.state.cam_id + ' camer is clickkkkkkeeeedddd');
+                    }.bind(this))
+                }.bind(this)}></DropDownItem>
               </DropdownMenu>
             </Dropdown>
           </Col>
           <Col>
-            <DatePickerInput style={{marginRight: 500 + 'px' }}/>
+            <Button color="primary" onClick={function(){
+              //console.log(this.state.date.getDate().toString);
+              this.setState({
+                dropdownOpen: this.state.dropdownOpen,
+                dropDownValue: this.state.dropDownValue,
+                list: this.state.list,
+                cam_id: this.state.cam_id,
+                date: this.state.date,
+                bar : {
+                  labels: this.state.bar.labels,
+                  datasets: [
+                    {
+                      label: '유동인구 수',
+                      backgroundColor: 'rgba(255,99,132,0.2)',
+                      borderColor: 'rgba(255,99,132,1)',
+                      borderWidth: 1,
+                      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                      hoverBorderColor: 'rgba(255,99,132,1)',
+                      data: this.state.data,
+                    },
+                  ],
+                }
+              })
+            }.bind(this)}>선택</Button>
           </Col>
         </Row>
           
           <Card style={{marginTop: 40 + 'px' }}>
             <CardHeader>
-              여기 차트제목
-              <div className="card-header-actions">
-                <a href="http://www.chartjs.org" className="card-header-action">
-                  <small className="text-muted">docs</small>
-                </a>
-              </div>
+              {this.state.dropDownValue}
             </CardHeader>
             <CardBody>
               <div className="chart-wrapper">
-                <Bar data={bar} options={options} height = {300}/>
+                <Bar data={this.state.bar} options={options} height = {300}/>
               </div>
             </CardBody>
           </Card>
-          {/*<Card>
-            <CardHeader>
-              Bar Chart
-              <div className="card-header-actions">
-                <a href="http://www.chartjs.org" className="card-header-action">
-                  <small className="text-muted">docs</small>
-                </a>
-              </div>
-            </CardHeader>
-            <CardBody>
-              <div className="chart-wrapper">
-                <Bar data={bar} options={options} />
-              </div>
-            </CardBody>
-          </Card>*/}
-          {/* <Card>
-            <CardHeader>
-              Doughnut Chart
-              <div className="card-header-actions">
-                <a href="http://www.chartjs.org" className="card-header-action">
-                  <small className="text-muted">docs</small>
-                </a>
-              </div>
-            </CardHeader>
-            <CardBody>
-              <div className="chart-wrapper">
-                <Doughnut data={doughnut} />
-              </div>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardHeader>
-              Radar Chart
-              <div className="card-header-actions">
-                <a href="http://www.chartjs.org" className="card-header-action">
-                  <small className="text-muted">docs</small>
-                </a>
-              </div>
-            </CardHeader>
-            <CardBody>
-              <div className="chart-wrapper">
-                <Radar data={radar} />
-              </div>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardHeader>
-              Pie Chart
-              <div className="card-header-actions">
-                <a href="http://www.chartjs.org" className="card-header-action">
-                  <small className="text-muted">docs</small>
-                </a>
-              </div>
-            </CardHeader>
-            <CardBody>
-              <div className="chart-wrapper">
-                <Pie data={pie} />
-              </div>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardHeader>
-              Polar Area Chart
-              <div className="card-header-actions">
-                <a href="http://www.chartjs.org" className="card-header-action">
-                  <small className="text-muted">docs</small>
-                </a>
-              </div>
-            </CardHeader>
-            <CardBody>
-              <div className="chart-wrapper">
-                <Polar data={polar} options={options}/>
-              </div>
-            </CardBody>
-          </Card> */}
+          
       </div>
     );
   }
