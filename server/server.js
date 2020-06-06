24ec52c933ad4f1b45a791b62959843b99d21cab
pse71 <psepse7171@gamil.com>
2020-06-07 01:39:04 +0900

    흑ㅎ헥ㄱ,,,, 수정
    수정했어욤 - 파일에서 필요한 값들 그룹화해서 sum구했음

--- server/server.js
+++ server/server.js
@@ -34,114 +34,156 @@ mongoose.connect('mongodb+srv://cdp12:cdp12@cdp12-eqgsf.mongodb.net/test?retryWr
     
 });
 
-var moment = require('moment-timezone');
-moment().tz("Asia/Seoul").format();
-var datee = moment().format('YYYY-MM-DD HH'); //년 월 일 시간만! 나타난다.
-
 var population = mongoose.Schema({ //보드에서 들어오는 유동인구 측정 데이터를 위한 스키마 생성
     id: {
-        type: Number,
-        required: true,
-      },
-    count: {
-        type: Number,
-        required: true,
+        type: String,
       },
     date: {
         type: String,
-      },
-    sign: {
+    },
+    time: {
         type: String,
-    }
+      },
+    count: {
+        type: String,
+      }
 })
-
 
 module.exports = mongoose.model('p_data',population);//유동인구
 
 var curpop = mongoose.model('p_data',population); //피플스키마의 모델 생성
 
-<<<<<<< Updated upstream
-var test= new curpop({ 
-    id: 1,
-    count: 10
-=======
-//그냥 아무 값으로 설정 client에서 값 받아오면 여기에 저장해서 갖다가 사용할 예정...
-//보드에서 받아온값 저장할때 게속 사용하면됨 다 저장하고 나면 주석처리하든 맨마지막에서 저장을 막든
-var board_id = 1;
-var board_countt =30;
+var arr = [ 
+    { time: "22", id: "1", count: "5" , date: "200606"},
+    { time: "22", id: "1", count: "10", date: "200607"},
+    { time: "22", id: "2", count: "15" , date: "200606"},
+    { time: "22", id: "2", count: "20", date: "200607"},
+    { time: "23", id: "1", count: "25" , date: "200606"},
+    { time: "23", id: "1", count: "30" , date: "200607"},
+    { time: "23", id: "2", count: "35",  date: "200606"},
+    { time: "23", id: "2", count: "40" , date: "200607"}
+];
 
-var test= new curpop({ 
-    id: board_id,
-    count: board_countt,
-    date : datee,
-    sign : null
->>>>>>> Stashed changes
+var groupBy = (arr, time, id='', date='') => {
+
+var timeArr = [];
+var resultArr = [];
+
+arr.map((item)=>{
+ var pushed = false;
+ timeArr.map((ele)=>{
+   if(ele===item.time){
+     pushed = true;
+   }
+ })
+ if(!pushed){
+   timeArr.push(item.time);
+ }     
 })
-//////////////////////////////////////////////////
 
-var sum1 = 0, sum2=0, hourlyid1 = 0, hourlyid2 = 0;
-//한시간 마다 원하는 시간을 입력해 주면 됨,,,
-// 같은 id끼리 그룹을 만들어서 count들을 다 합쳐줬음
-curpop.aggregate([
-    { $match: { date : "2020-05-23 05" } },
-    { $match: { sign : null }},
-    { $group: {_id: "$id", total: {$sum: "$count"}}}
-], function (err, result) {
-    if (err) return handleError(err);
-    console.log(result);
-    sum1=result[0].total;
-    sum2 = result[1].total;
-    hourlyid1 = result[0]._id;
-    hourlyid2 = result[1]._id;
-    //console.log(sum1); 
-    //console.log(sum2);  
+timeArr.map((item)=>{
+  var sum = 0;
+  arr.map((ele)=>{
+    if(ele.time===item){
+      sum += parseFloat(ele.count)
+    }
+  })
+  resultArr.push({
+    time: item,
+    count: sum
+  })
+})
 
-    /*1시간마다 같은 id의 인식 갯수 count가 모두 더해져서 id, 계산된 count값, 시간, 
-    sign(나중에 웹에서 원하는 시간대 정보를 가져갈때 디비에서 1시간마다 계산된 결과 값만을 대상으로 검색하기 위해서 구분용)*/
-    //모듈의 갯수만큼 저장해줍니당
-    curpop.create({
-        id: hourlyid1,
-        count: sum1,
-        date : datee,
-        sign : "hour"
-    },function(err){
-        if(err) return console.log("결과값 저장에러");
+if(id!=''){
+ var resultArr = [];
+
+
+ timeArr.map((item)=>{
+     var idArr = [];
+
+     arr.map((item2)=>{
+       var pushed = false;
+       idArr.map((ele)=>{
+         if(ele===item2.id){
+           pushed = true;
+         }
+       })
+       if(!pushed){
+         idArr.push(item2.id);
+       } 
+     })
+
+     idArr.map((item1)=>{
+        var sum = 0;
+        arr.map((ele)=>{
+          if(ele.id===item1 && ele.time===item){
+            sum += parseFloat(ele.count)
+          }
+        })
+        resultArr.push({
+          time: item,
+          id: item1,
+          count: sum,
+        })
+     })
+
+ })
+ return resultArr;
+} 
+/*if(date!=''){
+    var resultArr = [];
+   
+   
+    timeArr.map((item)=>{
+        var idArr = [];
+   
+        arr.map((item3)=>{
+          var pushed = false;
+          idArr.map((ele)=>{
+            if(ele===item3.date){
+              pushed = true;
+            }
+          })
+          if(!pushed){
+            idArr.push(item3.date);
+          } 
+        })
+   
+        idArr.map((item1)=>{
+           var sum = 0;
+           arr.map((ele)=>{
+             if(ele.date===item1 && ele.time===item){
+                resultArr.push({
+                    time: item,
+                    id: item1,
+                    count: sum,
+                  })
+            }
+           })
+        })
+   
     })
+    return resultArr;
+   }*/
+return resultArr;
+}
 
-    curpop.create({
-        id: hourlyid2,
-        count: sum2,
-        date : datee,
-        sign : "hour"
-    },function(err){
-        if(err) return console.log("결과값 저장에러");
-    })
-//////////////////////////////////////////////////////////
-});
+var result = groupBy(arr, 'Phase', 'Step');
+console.log(result[0].id);
 
-var choiceid = 0, choicecount = 0;
-//hour즉 1시간자리 통계인것만 db에서 들고와서 var에 저장 밑 콘솔 출력 (원하는 시간 입력해야함..)
-curpop.aggregate([
-    { $match: { sign: "hour" } },
-    { $match: { date: "2020-05-23 05" }},
- ], function (err, result) {
-    if (err) return console.log(err);
-    choiceid = result[result.length-2].id;
-    choicecount = result[result.length-2].count;
-    console.log("선택된 시간대의 첫번째 id = ",choiceid); 
-    console.log("선택된 시간대의 첫번째 count = ",choicecount); 
-} )
 
-test.save(function(err, test){ //DB에 저장 이걸 막아놔도 create는 못막지만 사용시 접근을 id갯수만큼만 하니까 그닥 상관 없음
-    if(err) return console.log("디비에 저장에러");
-    //console.dir(test)
-})
+ for(var i=0; i<result.length; i++){
+        var test= new curpop({ 
+            id: result[i].id,
+            time : result[i].time,
+            count: result[i].count,
+        })
+        console.log(test);
+        /*test.save(function(err, test){ //DB에 저장 이걸 막아놔도 create는 못막지만 사용시 접근을 id갯수만큼만 하니까 그닥 상관 없음
+            if(err) return console.log("디비에 저장에러");
+            //console.dir(test)
+        })*/
+    }
 
-//값을 넣으면서 바로 그 값을 반영할수는 없음 DB에 왔다 갔다 해야 정보를 알 수 있으니까....
 
-<<<<<<< Updated upstream
-    //console.dir(pse);
-})
 
-=======
->>>>>>> Stashed changes
