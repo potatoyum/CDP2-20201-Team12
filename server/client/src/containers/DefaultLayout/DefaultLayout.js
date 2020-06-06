f6d16e73d831dc0b2378a95258eca41665c0105e
YeongwonHam <yeomgg012@gmail.com>
2020-06-06 21:04:33 +0900

    웹 수정

--- server/client/src/containers/DefaultLayout/DefaultLayout.js
+++ server/client/src/containers/DefaultLayout/DefaultLayout.js
@@ -20,7 +20,6 @@ import navigation from '../../_nav';
 // routes config
 import routes from '../../routes';
 
-const DefaultAside = React.lazy(() => import('./DefaultAside'));
 const DefaultFooter = React.lazy(() => import('./DefaultFooter'));
 const DefaultHeader = React.lazy(() => import('./DefaultHeader'));
 
@@ -28,17 +27,12 @@ class DefaultLayout extends Component {
 
   loading = () => <div className="animated fadeIn pt-1 text-center">Loading...</div>
 
-  signOut(e) {
-    e.preventDefault()
-    this.props.history.push('/login')
-  }
-
   render() {
     return (
       <div className="app">
         <AppHeader fixed style={{border: "none"}}>
           <Suspense  fallback={this.loading()}>
-            <DefaultHeader onLogout={e=>this.signOut(e)}/>
+            <DefaultHeader/>
           </Suspense>
         </AppHeader>
         <div className="app-body">
@@ -72,11 +66,6 @@ class DefaultLayout extends Component {
               </Suspense>
             </Container>
           </main>
-          <AppAside fixed>
-            <Suspense fallback={this.loading()}>
-              <DefaultAside />
-            </Suspense>
-          </AppAside>
         </div>
         <AppFooter>
           <Suspense fallback={this.loading()}>
